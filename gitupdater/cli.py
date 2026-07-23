82fadd32d8c5c9532ee66987fd2dd46cb0ea00f2
Ayakura Yuki <YukiAyakura@outlook.com>
2026-07-23 09:16:57 +0800

    @ upgrade

--- gitupdater/cli.py
+++ gitupdater/cli.py
@@ -13,9 +13,9 @@ from gitupdater.update import update_directories
 
 
 def _build_parser():
-    """build and return the argument parser"""
+    """Build and return the argument parser."""
     parser = argparse.ArgumentParser(
-        description="easily update multiple git repositories at once",
+        description="Easily update multiple git repositories at once.",
         epilog="Both relative and absolute paths are accepted by all arguments.",
         add_help=False,
     )
@@ -23,15 +23,13 @@ def _build_parser():
     group_u = parser.add_argument_group("updating repositories")
     group_m = parser.add_argument_group("miscellaneous")
 
-    # directories_to_update
     group_u.add_argument(
         "directories_to_update",
         nargs="*",
         metavar="path",
-        help="""update this repository, or all repositories in contains 
-                if not a repo directly""",
+        help="""update this repository, or all repositories it contains
+                (if not a repo directly)""",
     )
-    # max_depth: -t --depth
     group_u.add_argument(
         "-t",
         "--depth",
@@ -39,59 +37,50 @@ def _build_parser():
         metavar="n",
         type=int,
         default=3,
-        help="""max recursion depth when searching for repositories in 
-                subdirectories, default is 3, use 0 fo no recursion, 
-                or -1 for unlimited""",
+        help="""max recursion depth when searching for repos in subdirectories
+                (default: 3; use 0 for no recursion, or -1 for unlimited)""",
     )
-    # -c --current-only
     group_u.add_argument(
         "-c",
         "--current-only",
         action="store_true",
-        help="""only fetch the remote tracked by the current branch 
-                instead of all remotes""",
+        help="""only fetch the
+                remote tracked by the current branch instead of all remotes""",
     )
-    # -f --fetch-only
     group_u.add_argument(
         "-f",
         "--fetch-only",
         action="store_true",
-        help="""only fetch remotes, don't try to fast-forward any branches""",
+        help="only fetch remotes, don't try to fast-forward any branches",
     )
-    # -p --prune
     group_u.add_argument(
         "-p",
         "--prune",
         action="store_true",
-        help="""after fetching, delete remote-tracking branches that 
-                no longer exist on their remote""",
+        help="""after fetching, delete
+            remote-tracking branches that no longer exist on their remote""",
     )
 
-    # -h --help
     group_m.add_argument(
         "-h", "--help", action="help", help="show this help message and exit"
     )
-    # -v --version
     group_m.add_argument(
         "-v",
         "--version",
         action="version",
-        version="git-updater {0} (Python {1})".format(
-            __version__, platform.python_version()
-        ),
+        version="git-updater {0} (Python {1})".format(__version__, platform.python_version()),
     )
-    # --self-test
     group_m.add_argument(
-        "--self-test",
+        "--selftest",
         action="store_true",
-        help="run integrated test suite and exit (required pytest)",
+        help="run integrated test suite and exit (pytest must be available)",
     )
 
     return parser
 
 
-def _self_test():
-    """run the integrated test suite with pytest"""
+def _selftest():
+    """Run the integrated test suite with pytest."""
     from .test import run_tests
 
     run_tests()
@@ -103,16 +92,11 @@ def main():
     color_init(autoreset=True)
     args = parser.parse_args()
 
-    print(
-        Style.BRIGHT
-        + "[git-updater]"
-        + Style.RESET_ALL
-        + ": the git-repo-updater without bookmark"
-    )
+    print(Style.BRIGHT + "[git-updater]" + Style.RESET_ALL + ": the git-repo-updater")
     print()
 
-    if args.self_test:
-        _self_test()
+    if args.selftest:
+        _selftest()
         return
 
     _acted = False
@@ -122,11 +106,11 @@ def main():
 
 
 def run():
-    """Thin wrapper for main()"""
+    """Thin wrapper for main() that catches KeyboardInterrupts."""
     try:
         main()
     except KeyboardInterrupt:
-        print("stopped by user")
+        print("Stopped by user.")
 
 
 if __name__ == "__main__":
@@ -134,4 +118,4 @@ if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        print("stopped by user")
+        print("Stopped by user.")
--- gitupdater/update.py
+++ gitupdater/update.py
@@ -10,6 +10,7 @@ import os.path
 import re
 import shlex
 from glob import glob
+from typing import AnyStr
 
 from colorama import Fore, Style
 from git import Repo, exc, RemoteReference as RemoteRef, Head
@@ -29,93 +30,42 @@ ERROR = RED + "Error:" + RESET
 
 
 class _ProgressMonitor(RemoteProgress):
-    """display relevant output during the fetching progress"""
+    """Displays relevant output during the fetching process."""
 
     def __init__(self):
         super(_ProgressMonitor, self).__init__()
         self._started = False
 
     def update(self, op_code, cur_count, max_count=None, message=""):
-        """called whenever progress changes
-
-        overrides default behavior"""
+        """Called whenever progress changes. Overrides default behavior."""
         if op_code & (self.COMPRESSING | self.RECEIVING):
             cur_count = str(int(cur_count))
-
             if max_count:
                 max_count = str(int(max_count))
             if op_code & self.BEGIN:
                 print("\b, " if self._started else " (", end="")
                 if not self._started:
                     self._started = True
-
             if op_code & self.END:
                 end = ")"
             elif max_count:
                 end = "\b" * (1 + len(cur_count) + len(max_count))
             else:
                 end = "\b" * len(cur_count)
-
             if max_count:
                 print("{0}/{1}".format(cur_count, max_count), end=end)
             else:
                 print(str(cur_count), end=end)
 
 
-def _update_repository(repo: Repo, repo_name, args):
-    """Update a single git repository by fetching remotes and rebasing/merging.
-
-    The specific actions depend on the arguments given, we will fetch all
-    remotes if *args.current_only* is ``False``, or only the remote tracked by
-    the current branch if ``True``.
-
-    If *args.fetch_only* is ``False``, we will also update all fast-forwardable
-    branches that are tracking valid upstreams.
-
-    If *args.prune* is ``True``, remote-tracking branches that no longer exist
-    on their remote after fetching will be deleted.
-    """
-
-    print(INDENT1, BOLD + repo_name + ":")
-
-    try:
-        active = repo.active_branch
-    except TypeError:
-        active = None
-
-    if args.current_only:
-        if not active:
-            print(
-                INDENT2,
-                ERROR,
-                "--current-only does not make sense with a detached HEAD",
-            )
-            return
-        ref = active.tracking_branch()
-        if not ref:
-            print(INDENT2, ERROR, "no remote tracked by current branch")
-            return
-        remotes = [repo.remotes[ref.remote_name]]
-    else:
-        remotes = repo.remotes
-
-    if not remotes:
-        print(INDENT2, ERROR, "no remotes configured to fetch")
-        return
-    _fetch_remotes(remotes, args.prune)
-
-    if not args.fetch_only:
-        for branch in sorted(repo.heads, key=lambda b: b.name):
-            _update_branch(repo, branch, branch == active)
-
-
 def _fetch_remotes(remotes, prune):
-    """fetch a list of remotes, displaying progress info along the way"""
+    """Fetch a list of remotes, displaying progress info along the way."""
 
     def _get_name(ref):
-        """return the local name of a remote or tag reference"""
+        """Return the local name of a remote or tag reference."""
         return ref.remote_head if isinstance(ref, RemoteRef) else ref.name
 
+    # TODO: missing branch deleted (via --prune):
     info = [
         ("NEW_HEAD", "new branch", "new branches"),
         ("NEW_TAG", "new tag", "new tags"),
@@ -127,13 +77,13 @@ def _fetch_remotes(remotes, prune):
         print(INDENT2, "Fetching", BOLD + remote.name, end="")
 
         if not remote.config_reader.has_option("fetch"):
-            print(":", YELLOW + "skipping", "no configured refspec")
+            print(":", YELLOW + "skipped:", "no configured refspec.")
             continue
 
         try:
             results = remote.fetch(progress=_ProgressMonitor(), prune=prune)
         except exc.GitCommandError as err:
-            # we should have to do this ourselves but GitPython does not give
+            # We should have to do this ourselves, but GitPython doesn't give
             # us a sensible way to get the raw stderr...
             msg = re.sub(r"\s+", " ", err.stderr).strip()
             msg = re.sub(r"^stderr: *'(fatal: *)?", "", msg).strip("'")
@@ -144,17 +94,15 @@ def _fetch_remotes(remotes, prune):
                 msg += "."
             print(":", RED + "error:", msg)
             return
-        except AssertionError:
-            # seems to be the result of a bug in GitPython
-            # this happens when git initiates an auto-gc during fetch
+        except AssertionError:  # Seems to be the result of a bug in GitPython
+            # This happens when git initiates an auto-gc during fetch:
             print(
                 ":",
                 RED + "error:",
-                "something went wrong in GitPython, "
-                "but the fetch might have been successful",
+                "something went wrong in GitPython,",
+                "but the fetch might have been successful.",
             )
             return
-
         rlist = []
         for attr, singular, plural in info:
             names = [
@@ -168,7 +116,7 @@ def _fetch_remotes(remotes, prune):
 
 
 def _update_branch(repo: Repo, branch: Head, is_active=False):
-    """update a single branch"""
+    """Update a single branch."""
     print(INDENT2, "Updating", BOLD + branch.name, end=": ")
     upstream = branch.tracking_branch()
     if not upstream:
@@ -188,7 +136,7 @@ def _update_branch(repo: Repo, branch: Head, is_active=False):
     try:
         base = repo.git.merge_base(branch.commit, upstream.commit)
     except exc.GitCommandError:
-        print(YELLOW + "skipped:", "cannot find merge base with upstream.")
+        print(YELLOW + "skipped:", "can't find merge base with upstream.")
         return
 
     if repo.commit(base) == upstream.commit:
@@ -220,6 +168,48 @@ def _update_branch(repo: Repo, branch: Head, is_active=False):
             print(GREEN + "done", end=".\n")
 
 
+def _update_repository(repo: Repo, repo_name, args):
+    """Update a single git repository by fetching remotes and rebasing/merging.
+
+    The specific actions depend on the arguments given. We will fetch all
+    remotes if *args.current_only* is ``False``, or only the remote tracked by
+    the current branch if ``True``. If *args.fetch_only* is ``False``, we will
+    also update all fast-forwardable branches that are tracking valid
+    upstreams. If *args.prune* is ``True``, remote-tracking branches that no
+    longer exist on their remote after fetching will be deleted.
+    """
+    print(INDENT1, BOLD + repo_name + ":")
+
+    try:
+        active = repo.active_branch
+    except TypeError:  # Happens when HEAD is detached
+        active = None
+    if args.current_only:
+        if not active:
+            print(
+                INDENT2,
+                ERROR,
+                "--current-only doesn't make sense with a detached HEAD.",
+            )
+            return
+        ref = active.tracking_branch()
+        if not ref:
+            print(INDENT2, ERROR, "no remote tracked by current branch.")
+            return
+        remotes = [repo.remotes[ref.remote_name]]
+    else:
+        remotes = repo.remotes
+
+    if not remotes:
+        print(INDENT2, ERROR, "no remotes configured to fetch.")
+        return
+    _fetch_remotes(remotes, args.prune)
+
+    if not args.fetch_only:
+        for branch in sorted(repo.heads, key=lambda b: b.name):
+            _update_branch(repo, branch, branch == active)
+
+
 def _dispatch(base_path, callback, args):
     """Apply a callback function on each valid repo in the given path.
 
@@ -232,7 +222,7 @@ def _dispatch(base_path, callback, args):
     """
 
     def _collect(_paths, _max_depth):
-        """return all valid repo paths in the given paths, recursively"""
+        """Return all valid repo paths in the given paths, recursively."""
         if _max_depth == 0:
             return []
 
@@ -244,14 +234,14 @@ def _dispatch(base_path, callback, args):
             except exc.InvalidGitRepositoryError:
                 if not os.path.isdir(path):
                     continue
-                children = [os.path.join(path, v) for v in os.listdir(path)]
+                children = [os.path.join(path, it) for it in os.listdir(path)]
                 _valid += _collect(children, _max_depth - 1)
             except exc.NoSuchPathError:
                 continue
         return _valid
 
     def _get_basename(_base, path):
-        """return a reasonable name for a repo path in the given base"""
+        """Return a reasonable name for a repo path in the given base."""
         if path.startswith(_base + os.path.sep):
             return path.split(_base + os.path.sep, 1)[1]
         prefix = os.path.commonprefix([_base, path])
@@ -259,7 +249,7 @@ def _dispatch(base_path, callback, args):
             old = prefix
             prefix = os.path.split(prefix)[0]
             if prefix == old:
-                break  # prevent infinite loop, but should be almost impossible, just protect
+                break  # Prevent infinite loop, but should be almost impossible
         return path.split(prefix + os.path.sep, 1)[1]
 
     base = os.path.expanduser(base_path)
@@ -270,11 +260,6 @@ def _dispatch(base_path, callback, args):
     try:
         Repo(base)
         valid = [base]
-    except exc.InvalidGitRepositoryError:
-        if not os.path.isdir(base) or args.max_depth == 0:
-            print(ERROR, BOLD + base, "is not a repository!!!")
-            return
-        valid = _collect([base], max_depth)
     except exc.NoSuchPathError:
         if is_comment(base):
             comment = get_comment(base)
@@ -283,9 +268,14 @@ def _dispatch(base_path, callback, args):
             return
         paths = glob(base)
         if not paths:
-            print(ERROR, BOLD + base, "does not exist!!!")
+            print(ERROR, BOLD + base, "doesn't exist!")
             return
         valid = _collect(paths, max_depth)
+    except exc.InvalidGitRepositoryError:
+        if not os.path.isdir(base) or args.max_depth == 0:
+            print(ERROR, BOLD + base, "isn't a repository!")
+            return
+        valid = _collect([base], max_depth)
 
     base = os.path.abspath(base)
     suffix = "" if len(valid) == 1 else "s"
@@ -297,17 +287,17 @@ def _dispatch(base_path, callback, args):
         callback(Repo(path), name, args)
 
 
-def is_comment(path: str):
-    """does the line start with a # symbol"""
+def is_comment(path):
+    """Does the line start with a # symbol?"""
     return path.lstrip().startswith("#")
 
 
-def get_comment(path: str):
-    """return the string minus the comment symbol"""
+def get_comment(path):
+    """Return the string minus the comment symbol."""
     return path.lstrip().lstrip("#").strip()
 
 
 def update_directories(paths, args):
-    """update a list of directories supplied by command arguments"""
+    """Update a list of directories supplied by command arguments."""
     for path in paths:
         _dispatch(path, _update_repository, args)
